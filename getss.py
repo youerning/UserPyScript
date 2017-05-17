4c6a4e7ccf591389a590f18ac5368918b3b79898
youer <673125641@qq.com>
2017-05-17 16:25:25 +0800

    updat getss.py

--- getss.py
+++ getss.py
@@ -1,34 +1,63 @@
-#coding:utf8
-#get the free shodowsocks account and save the information to qr png file
+# coding:utf8
+# get the free shodowsocks account and save the information to qr png file
 from base64 import b64encode
 from bs4 import BeautifulSoup
 import qrcode
 import requests
+import random
 
-ssURL = "http://www.ishadowsocks.org/"
-page = requests.get(ssURL)
-soup = BeautifulSoup(page.content,"html.parser")
+userAgents = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+]
 
-authALL = soup.findAll(class_="col-lg-4 text-center")
+ssURL = "http://ss.ishadowx.com"
+ct = ["us", "jp", "sg"]
+# ct = ["jp"]
+baseClass = "col-sm-6 col-md-4 col-lg-4 %s"
+bsCls = baseClass % random.choice(ct)
+header = {'User-Agent': random.choice(userAgents)}
+
+# get the page content
+page = requests.get(ssURL, headers=header)
+soup = BeautifulSoup(page.content, "html.parser")
+
 sourceLis = []
 destLis = []
+account = soup.findAll(class_=bsCls)
 
-for server in authALL[:3]:
+for a in account:
     lis = []
-    for info in server.children:
-        if info != "\n":
-                #print info.string
-                lis.append(info.string.split(":")[1])
-                if info.string.split(":")[1] == "aes-256-cfb":
-                    break
-    #authURL = ":".join(lis)
+    for i in a.findAll("h4"):
+        txt = i.text
+        if ":" in txt:
+            var = txt.split(":")[1]
+        elif u"：" in i.text:
+            var = txt.split(u"：")[1]
+        lis.append(var.strip())
+
+    # print lis
     authURL = lis[3] + ":" + lis[2] + "@" + lis[0] + ":" + lis[1]
     sourceLis.append(authURL)
-    destLis.append("ss://" + b64encode(authURL))
+    destLis.append("ss://" + b64encode(authURL)) 
 
 print sourceLis
 print destLis
 
-for i, j in zip(destLis,range(len(destLis))):
-    img = qrcode.make(i)
-    img.save(str(j) + ".png")
+for i, j in enumerate(destLis):
+    img = qrcode.make(j)
+    img.save(str(i) + ".png")
